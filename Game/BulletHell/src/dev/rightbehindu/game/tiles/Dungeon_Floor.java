b13638b0ac7a4d391c4136c89db987ecaf0889c6
RightBehindu <pianomanz@hotmail.com>
2017-08-05 17:23:21 +0800

    Starting World Alpha
    Began implementing Asset, Tile and World code. All bare bones and ready to be changed.

---
+++ Game/BulletHell/src/dev/rightbehindu/game/tiles/Dungeon_Floor.java
@@ -0,0 +1,25 @@
+package dev.rightbehindu.game.tiles;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Dungeon_Floor extends Tile {
+
+	public Dungeon_Floor(BufferedImage texture, int id) {
+		super(texture, id);
+	}
+
+	@Override
+	public boolean isSolid() {
+		return false;
+	}
+
+	@Override
+	public void render(Graphics g, int x, int y) {
+		System.out.println("KILL ME");
+		g.drawImage(this.texture, x * TILE_SIZE, y * TILE_SIZE, null);
+	}
+
+
+
+}
